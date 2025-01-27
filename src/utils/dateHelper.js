fcd465e50dc468ef8afe0905f29f2ac5787fe12e
Reyansh <reyanshjoshi4511@gmail.com>
2025-01-28 01:13:12 +0530

    sdd

--- src/utils/dateHelper.js
+++ src/utils/dateHelper.js
@@ -1,12 +1,14 @@
 const changeDateTimeToNight = (date) => {
   const newDate = new Date(date);
-  newDate.setUTCHours(0, 0, 0, 0);
+  // Adjust for the offset (-5 hours and 30 minutes)
+  newDate.setUTCHours(0 - 5, 0 - 30, 0, 0); // Add offset to shift the time correctly
   return newDate;
 };
 
 const changeDateTimeToNightAfter = (date) => {
   const newDate = new Date(date);
-  newDate.setUTCHours(23, 59, 59, 999);
+  // Adjust for the offset (-5 hours and 30 minutes)
+  newDate.setUTCHours(23 - 5, 59 - 30, 59, 999); // Add offset to shift the time correctly
   return newDate;
 };
 
